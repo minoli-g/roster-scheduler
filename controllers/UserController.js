39b378db527b2bef5d807eb2f84b22c0d4e8e092
Minoli <minoliniroshani@gmail.com>
2021-08-18 12:42:29 +0530

    Password checking added

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -1,3 +1,5 @@
+bcrypt = require('bcrypt');
+
 user = require('../models/User');
 
 class UserController{
@@ -7,8 +9,7 @@ class UserController{
     }
 
     static homePage(req,res){
-        res.render('home', {username: req.session.user});
-        console.log(req.session.user);
+        res.render('home', {username: req.session.user.username});
     }
 
     static async login(req,res){
@@ -17,15 +18,39 @@ class UserController{
         if validation/verification failed, display error
         else redirect to home page*/
 
-        const user_info = await user.getUserByUsername(req.body.username);
+        var user_info = await user.getUserByUsername(req.body.username);
+        user_info = user_info[0];
 
         if (!user_info) {
             //throw error saying no such username
+            res.render('login',{message: "This user does not exist"});
+            return;
         }
-        req.session.user = user_info[0].username;
+
+        const match = await bcrypt.compare(req.body.password,user_info.password);
+
+        if(!match) {
+            //throw error saying incorrect password
+            res.render('login',{message: "Incorrect password"});
+            return;
+        }
+
+        //Store all the user info in the session
+        req.session.user = {};  //clear session data
+        req.session.user.id = user_info.user_id;
+        req.session.user.username = user_info.username;
+        req.session.user.first_name = user_info.first_name;
+        req.session.user.last_name = user_info.last_name;
+        req.session.user.type = user_info.type;
 
         //res.send("home");
         res.redirect('/home');
+    }
+
+    static async logout(req,res){
+
+        req.session.user = undefined;
+        res.redirect('/');
     }
 }
 
