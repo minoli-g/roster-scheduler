8f8ad3201547d72d97b7e34e93b9079c509cdf60
godfrey06 <victorjeyakumar.18@cse.mrt.ac.lk>
2021-11-08 17:50:11 +0530

    Last Updated

--- config/db.js
+++ config/db.js
@@ -1,10 +1,14 @@
 var mysql = require('mysql')
 
 var connection = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: '',  //change this to local machine's password
-  database: 'schedular'
+  // host: 'localhost',
+  // user: 'root',
+  // password: '',  //change this to local machine's password
+  // database: 'schedular'
+  host: 'us-cdbr-east-04.cleardb.com',
+  user: 'b654e8927ee26b',
+  password: '090cb62d',  //change this to local machine's password
+  database: 'heroku_8a6f7a873a84775'
 })
 
 connection.connect();
