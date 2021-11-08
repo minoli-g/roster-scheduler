dfc0e46a798ba396e24bff9626698eefa52a2611
godfrey06 <victorjeyakumar.18@cse.mrt.ac.lk>
2021-11-08 22:03:35 +0530

    Last Updated

--- config/db.js
+++ config/db.js
@@ -9,15 +9,15 @@ database: 'heroku_8a6f7a873a84775'
 var connection;
 
 function handleDisconnect() {
-  connection = mysql.createConnection(db_config); // Recreate the connection, since
+  connection = mysql.createPool(db_config); // Recreate the connection, since
                                                   // the old one cannot be reused.
 
-  connection.connect(function(err) {              // The server is either down
-    if(err) {                                     // or restarting (takes a while sometimes).
-      console.log('error when connecting to db:', err);
-      setTimeout(handleDisconnect, 2000); // We introduce a delay before attempting to reconnect,
-    }                                     // to avoid a hot loop, and to allow our node script to
-  });                                     // process asynchronous requests in the meantime.
+  // connection.connect(function(err) {              // The server is either down
+  //   if(err) {                                     // or restarting (takes a while sometimes).
+  //     console.log('error when connecting to db:', err);
+  //     setTimeout(handleDisconnect, 2000); // We introduce a delay before attempting to reconnect,
+  //   }                                     // to avoid a hot loop, and to allow our node script to
+  // });                                     // process asynchronous requests in the meantime.
                                           // If you're also serving http, display a 503 error.
   connection.on('error', function(err) {
     console.log('db error', err);
