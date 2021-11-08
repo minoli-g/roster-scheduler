46a9e0037ebb82fefc634ae17e30a58c24e633f6
godfrey06 <victorjeyakumar.18@cse.mrt.ac.lk>
2021-11-08 20:49:16 +0530

    Last Updated

--- config/db.js
+++ config/db.js
@@ -1,16 +1,34 @@
 var mysql = require('mysql')
 
-var connection = mysql.createConnection({
-  // host: 'localhost',
-  // user: 'root',
-  // password: '',  //change this to local machine's password
-  // database: 'schedular'
-  host: 'us-cdbr-east-04.cleardb.com',
-  user: 'b654e8927ee26b',
-  password: '090cb62d',  //change this to local machine's password
-  database: 'heroku_8a6f7a873a84775'
-})
+const db_config = {
+host: 'us-cdbr-east-04.cleardb.com',
+user: 'b654e8927ee26b',
+password: '090cb62d',  //change this to local machine's password
+database: 'heroku_8a6f7a873a84775'
+}
+var connection;
 
-connection.connect();
+function handleDisconnect() {
+  connection = mysql.createConnection(db_config); // Recreate the connection, since
+                                                  // the old one cannot be reused.
+
+  connection.connect(function(err) {              // The server is either down
+    if(err) {                                     // or restarting (takes a while sometimes).
+      console.log('error when connecting to db:', err);
+      setTimeout(handleDisconnect, 2000); // We introduce a delay before attempting to reconnect,
+    }                                     // to avoid a hot loop, and to allow our node script to
+  });                                     // process asynchronous requests in the meantime.
+                                          // If you're also serving http, display a 503 error.
+  connection.on('error', function(err) {
+    console.log('db error', err);
+    if(err.code === 'PROTOCOL_CONNECTION_LOST') { // Connection to the MySQL server is usually
+      handleDisconnect();                         // lost due to either server restart, or a
+    } else {                                      // connnection idle timeout (the wait_timeout
+      throw err;                                  // server variable configures this)
+    }
+  });
+}
+
+handleDisconnect();
 
 module.exports = connection;
