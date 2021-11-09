9c130929f8f2c5401a4cb487cac1e110445313af
godfrey06 <victorjeyakumar.18@cse.mrt.ac.lk>
2021-11-09 15:18:31 +0530

    Last Updated

--- app.js
+++ app.js
@@ -1,77 +1,78 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+var createError = require("http-errors");
+var express = require("express");
+var path = require("path");
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
 var app = express();
-const cors= require('cors');
-require('dotenv').config({path:__dirname+'/env/.env.dev'});
+const cors = require("cors");
+require("dotenv").config({ path: __dirname + "/env/.env.prod" });
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "jade");
 
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-
-
+app.use(express.static(path.join(__dirname, "public")));
 
 // setting up session data storage
-const session=require("express-session");
-const MySQLStore=require('express-mysql-session')(session);
+const session = require("express-session");
+const MySQLStore = require("express-mysql-session")(session);
 
-var connection = require('./config/db');
-var sessionStore = new MySQLStore({}/* session store options */, connection);
+var connection = require("./config/db");
+var sessionStore = new MySQLStore({} /* session store options */, connection);
 
-app.use(session({
-	key: 'session_cookie_name',
-	secret: 'session_cookie_secret',
-	store: sessionStore,
-	resave: false,
-	saveUninitialized: false,
-  cookie: { maxAge: 10 * 24 * 60 * 60 * 1000 }
-}));
+app.use(
+  session({
+    key: "session_cookie_name",
+    secret: "session_cookie_secret",
+    store: sessionStore,
+    resave: false,
+    saveUninitialized: false,
+    cookie: { maxAge: 10 * 24 * 60 * 60 * 1000 },
+  })
+);
 
-
-var whitelist = [process.env.FRONTEND_BASE_URL, undefined, process.env.API_BASE_URL]
+var whitelist = [
+  process.env.FRONTEND_BASE_URL,
+  undefined,
+  process.env.API_BASE_URL,
+];
 var corsOptions = {
   origin: function (origin, callback) {
     console.log(origin);
     if (whitelist.indexOf(origin) !== -1) {
-      callback(null, true)
+      callback(null, true);
     } else {
-      callback(new Error('Not allowed by CORS'))
+      callback(new Error("Not allowed by CORS"));
     }
   },
-  methods : "*",
-  credentials : true
-}
+  methods: "*",
+  credentials: true,
+};
 
 // Then pass them to cors:
 app.use(cors(corsOptions));
 //Points to the index file of the routes folder, to guide to all routes.
-app.use(require('./routes'));
+app.use(require("./routes"));
 
 // app.use("/api/", require("./routes/doctorRoutes"))
 
-
-
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.error = req.app.get("env") === "development" ? err : {};
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.render("error");
 });
 
 module.exports = app;
--- config/db.js
+++ config/db.js
@@ -1,23 +1,23 @@
-var mysql = require('mysql')
-
-// const db_config = {
-// host: 'us-cdbr-east-04.cleardb.com',
-// user: 'b654e8927ee26b',
-// password: '090cb62d',  //change this to local machine's password
-// database: 'heroku_8a6f7a873a84775'
-// }
+var mysql = require("mysql");
 
 const db_config = {
-  host: 'localhost',
-  user: 'root',
-  password: '',  //change this to local machine's password
-  database: 'roster'
-  }
+  host: "us-cdbr-east-04.cleardb.com",
+  user: "b654e8927ee26b",
+  password: "090cb62d", //change this to local machine's password
+  database: "heroku_8a6f7a873a84775",
+};
+
+// const db_config = {
+//   host: 'localhost',
+//   user: 'root',
+//   password: '',  //change this to local machine's password
+//   database: 'roster'
+//   }
 var connection;
 
 function handleDisconnect() {
   connection = mysql.createPool(db_config); // Recreate the connection, since
-                                                  // the old one cannot be reused.
+  // the old one cannot be reused.
 
   // connection.connect(function(err) {              // The server is either down
   //   if(err) {                                     // or restarting (takes a while sometimes).
@@ -25,13 +25,15 @@ function handleDisconnect() {
   //     setTimeout(handleDisconnect, 2000); // We introduce a delay before attempting to reconnect,
   //   }                                     // to avoid a hot loop, and to allow our node script to
   // });                                     // process asynchronous requests in the meantime.
-                                          // If you're also serving http, display a 503 error.
-  connection.on('error', function(err) {
-    console.log('db error', err);
-    if(err.code === 'PROTOCOL_CONNECTION_LOST') { // Connection to the MySQL server is usually
-      handleDisconnect();                         // lost due to either server restart, or a
-    } else {                                      // connnection idle timeout (the wait_timeout
-      throw err;                                  // server variable configures this)
+  // If you're also serving http, display a 503 error.
+  connection.on("error", function (err) {
+    console.log("db error", err);
+    if (err.code === "PROTOCOL_CONNECTION_LOST") {
+      // Connection to the MySQL server is usually
+      handleDisconnect(); // lost due to either server restart, or a
+    } else {
+      // connnection idle timeout (the wait_timeout
+      throw err; // server variable configures this)
     }
   });
 }
